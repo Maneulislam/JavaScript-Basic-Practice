45cfb5e1f72ba34c729cd8e553764550edd20b7b
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-10-26 23:17:58 +0600

    first commit

---
+++ task.js
@@ -0,0 +1,31 @@
+
+var taka = 1000;
+console.log("Your taka : ", taka);
+
+var orangePrice = 400;
+var applePrice = 300;
+var totalPrice = orangePrice + applePrice;
+console.log("Total Price : ", totalPrice);
+
+var remaining = taka - totalPrice;
+console.log("Remaining Taka : ", remaining);
+
+
+
+
+
+// -------------------------------
+
+
+var marks = [5,10,15,20];
+
+var average = (marks[0] + marks[1] + marks[2] + marks[3]) / marks.length;
+console.log("Average : ", average.toFixed(2));
+
+
+//---------------------------------
+
+var num1 = 25;
+var result = num1 % 2;
+
+console.log(result);
